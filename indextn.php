8e3573b61ef290bb228d45e75be8af4f94a76ec2
dave-barrett <41449892+dave-barrett@users.noreply.github.com>
2018-07-19 20:57:12 +0100

    Setting up

--- indextn.php
+++ indextn.php
@@ -1,3 +1,14 @@
 <?php
 
 ?>
+
+<html lang="en-GB">
+<head>
+<meta charset='utf-8'>
+<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+<link rel="stylesheet" href="_res/_css/master.css">
+</head>
+<!-- Sort above in to include when complete -->
+<body>
+</body>
+</html>
