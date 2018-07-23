576566da7a563122fb010e063ccdd7c2195cf10e
dave-barrett <41449892+dave-barrett@users.noreply.github.com>
2018-07-23 13:07:34 +0100

    Add files via upload

---
+++ REP/SQL Create Scripts.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE dbPortal;
+
+
+CREATE TABLE dbPortal.tblPortal (
+  p_id INT NOT NULL AUTO_INCREMENT,
+  p_globalmsg VARCHAR(255),
+  p_globalmsg_start DATETIME,
+  p_globalmsg_life INT(4),
+  p_mods INT(5),
+  
+);
+
+
+CREATE TABLE dbPortal.tblUsers (
+  u_id INT NOT NULL AUTO_INCREMENT,
+  u_firstname VARCHAR(75) NOT NULL,
+  u_lastname VARCHAR(75),
+  u_username VARCHAR(100),
+  u_password VARCHAR(255),
+  u_email VARCHAR(125),
+  u_lastlogon DATETIME 
+  u_loggedon INT(1),
+  u_active INT(1),
+  u_regdate DATETIME 
+  u_accesscode VARCHAR(25),
+  PRIMARY KEY (u_id)
+);
+
