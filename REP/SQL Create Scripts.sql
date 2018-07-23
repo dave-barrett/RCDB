736166558d7079035f45400ce474e922f909435c
dave-barrett <41449892+dave-barrett@users.noreply.github.com>
2018-07-23 13:08:35 +0100

    Update SQL Create Scripts.sql
    , error

--- REP/SQL Create Scripts.sql
+++ REP/SQL Create Scripts.sql
@@ -1,15 +1,13 @@
 CREATE DATABASE dbPortal;
 
-
 CREATE TABLE dbPortal.tblPortal (
   p_id INT NOT NULL AUTO_INCREMENT,
   p_globalmsg VARCHAR(255),
   p_globalmsg_start DATETIME,
   p_globalmsg_life INT(4),
-  p_mods INT(5),
-  
+  p_mods INT(5)
+  PRIMARY KEY (p_id)
 );
-
 
 CREATE TABLE dbPortal.tblUsers (
   u_id INT NOT NULL AUTO_INCREMENT,
